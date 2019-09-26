5233f591a1a7bbd38629d16a32e08705b348106d
requistp <requistp@gmail.com>
2019-09-25 22:19:39 -0700

    Add files via upload

---
+++ World1Fb/FormSystem.fs
@@ -0,0 +1,10 @@
+﻿module FormSystem
+open SystemManager
+
+type FormSystem(isActive:bool) =
+    inherit AbstractSystem(isActive,false) 
+   
+    override this.Initialize ecm = ecm
+
+    override this.Update dt ecm = 
+        ecm
