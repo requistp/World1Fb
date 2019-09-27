f0965b22a22fed96dfa06601e94cd68dfc6739b3
Phil <requistp@gmail.com>
2019-09-27 11:29:03 -0700

    again3

--- World1Fb/ChangeLogManager.fs
+++ World1Fb/ChangeLogManager.fs
@@ -3,13 +3,22 @@ open CommonGenericFunctions
 open Components
 
 [<Struct>]
-type FrameChangeLog(ccl:Map<uint32,ComponentChangeType list>, entityAdditions:Map<string,ComponentType list>, entityRemovals:uint32 list) =
+type FrameChangeLog(ccl:Map<uint32,ComponentChangeType list>, entityAdditions:Map<uint32,ComponentType list>, entityRemovals:uint32 list) =
     static member New = FrameChangeLog(Map.empty,Map.empty,List.empty)
     member this.ComponentChangeLog = ccl
     member this.EntityAdditions = entityAdditions
     member this.EntityRemovals = entityRemovals
 
-    member this.AddEntity(ct:ComponentType list) = 
+    member this.AddComponentChange (e:uint32) (cct:ComponentChangeType) =
+        match ccl.ContainsKey(e) with
+        | true -> FrameChangeLog(ccl.Add(e,cct::ccl.Item(e)), entityAdditions, entityRemovals)
+        | false -> FrameChangeLog(ccl.Add(e,[cct]), entityAdditions, entityRemovals)
+    member this.AddEntity (ctl:ComponentType list) = 
+        match ctl.IsEmpty with
+        | true -> Failure "ctl is empty"
+        | false -> Success FrameChangeLog(ccl, entityAdditions.Add(uint32 (entityAdditions.Count+1),ctl), entityRemovals)
+    member this.RemoveEntity (e:uint32) = 
+        FrameChangeLog(ccl, entityAdditions, e::entityRemovals)
 
 type ChangeLogManager() = 
     let mutable _fcl = FrameChangeLog.New
@@ -24,23 +33,15 @@ type ChangeLogManager() =
     //    FrameChangeLog(Map.empty, Map.empty, List.empty
 
     member this.AddEntity (ctl:ComponentType list) = 
-        match ctl.IsEmpty with
-        | true -> Failure "ctl is empty"
-        | false -> fcl <-
-                   Success "done"
-        
+        match _fcl.AddEntity with
+        | Failure x -> Failure x
+        | Success fcl -> _fcl <- fcl
+                         Success "added"
 
     member this.AddEntities_ViaTuple (i,ctl:ComponentType list) list =
         1
 
     member this.AddEntities_ViaComponentTypeList (ctl:ComponentType list) =
-
-        ctl |> List.indexed
-
-        let mutable i = maxEntityID
-        let mutable newmap = ecmap
-        let mutable newcd = compDict
-
         for ct in ctl do
             i <- i + 1u
             let ec = EntityComponent(i,ct)
